eaa354dd0d71f08bb3f4dd80ae9727ba8567cb6c
yurepo <linzhongyu1105@gmail.com>
2021-03-16 01:38:47 +0800

    add example

--- example/example.go
+++ example/example.go
@@ -1,30 +1,43 @@
 package main
 
-import(
+import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/aitay721822/gin-redis-ratelimiter"
+	"github.com/go-redis/redis/v8"
 	"net/http"
+	"time"
 )
+
+var ctx = context.Background()
 
 func main() {
 
 	router := gin.Default()
 
-	router.GET("/ping", func(context *gin.Context) {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	lm1 := ginredisratelimiter.NewRedisRateLimiter(ctx, "ping1", time.Minute, 10, client)
+	lm2 := ginredisratelimiter.NewRedisRateLimiter(ctx, "ping2", 2 * time.Minute, 5, client)
+
+	router.GET("/ping", lm1.Middleware(), func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
 			"status": "OK",
 			"message": "pong",
 		})
 	})
 
-	router.GET("/ping2", func(context *gin.Context) {
+	router.GET("/ping2", lm2.Middleware(), func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
 			"status": "OK",
 			"message": "pong pong",
 		})
 	})
-
 
 	err := router.Run(":8080")
 	if err != nil {
